1fa2bbdcc18f32c55b131a3d280213a6378a28aa
dbuaklee <dbuaklee@hotmail.com>
2018-07-31 00:29:40 +0700

    add cors

--- tcs4-backend/src/main/java/th/go/dss/tcs4/config/WebSecuritry.java
+++ tcs4-backend/src/main/java/th/go/dss/tcs4/config/WebSecuritry.java
@@ -1,13 +1,20 @@
 package th.go.dss.tcs4.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class WebSecuritry extends WebSecurityConfigurerAdapter {
 
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().authorizeRequests().anyRequest().authenticated();
+		http
+		.cors().and()
+		.httpBasic().and()
+		.authorizeRequests()
+			.antMatchers("/index.html", "/", "/home", "/login", "/test").permitAll()
+			.anyRequest().authenticated();
 	}
 }
--- tcs4-backend/src/main/java/th/go/dss/tcs4/controller/TestController.java
+++ tcs4-backend/src/main/java/th/go/dss/tcs4/controller/TestController.java
@@ -2,13 +2,25 @@ package th.go.dss.tcs4.controller;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class TestController {
+	
+	public static Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping("/test")
+	@CrossOrigin(origins = "*", maxAge = 3600,
+    allowedHeaders={"x-auth-token", "x-requested-with", "x-xsrf-token"})
 	  public Message home() {
+		
+		logger.error("/test is called");
+		
 	    return new Message("Hello World");
 	  }
 }
