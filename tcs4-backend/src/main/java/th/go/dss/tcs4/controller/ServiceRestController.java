e2c3fa8f01a62a31fbda3756292ab25f19639b43
dbuaklee <dbuaklee@hotmail.com>
2018-08-01 19:07:09 +0700

    add sort

--- tcs4-backend/src/main/java/th/go/dss/tcs4/controller/ServiceRestController.java
+++ tcs4-backend/src/main/java/th/go/dss/tcs4/controller/ServiceRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,10 +27,33 @@ public class ServiceRestController {
 	@RequestMapping("/TestMethod")
 	@CrossOrigin()
 	  public Page<TestMethod> findAllTestMethod(
-			  @RequestParam Integer pageNum, @RequestParam Integer pageSize) {
-		logger.error("/TestMethod is called");
+			  @RequestParam Integer pageNum, @RequestParam Integer pageSize, 
+			  @RequestParam String sortField, @RequestParam String sortDirection) {
+		logger.debug("/TestMethod is called");
+		logger.debug("sortField: " + sortField);
+		logger.debug("sortDirection: " + sortDirection);
+		
+		Sort sort =null;
+		
+		if(sortField.length() > 0) {
+			Direction direction;
+			if(sortDirection.equals("desc")) {
+				direction = Direction.DESC;
+			} else {
+				direction = Direction.ASC;
+			}
+			
+			sort = Sort.by(direction, sortField);
+		} else {
+			sort = Sort.by(Direction.ASC, "id");
+		}
+		
 		PageRequest pageRequest =
-	           PageRequest.of(pageNum, pageSize);
+	           PageRequest.of(pageNum, pageSize, sort);
+		
+		
+		
+		//Sort.by(direction, properties)
 		
 	    return entityService.findAllTestMethod(pageRequest);
 	  }
